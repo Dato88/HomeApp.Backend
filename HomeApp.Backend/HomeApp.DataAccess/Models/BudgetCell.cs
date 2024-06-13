fb0aaaac484ddaf18d2acbef78ed6d9cef1bc4af
Andrej Miller <andrej_miller@outlook.de>
2024-06-13 21:37:06 +0200

    added Year to BudgetCell and BudgetRow

--- HomeApp.Backend/HomeApp.DataAccess/Models/BudgetCell.cs
+++ HomeApp.Backend/HomeApp.DataAccess/Models/BudgetCell.cs
@@ -5,12 +5,6 @@ namespace HomeApp.DataAccess.Models
     public partial class BudgetCell : BaseClass
     {
         [Required]
-        public int UserId { get; set; }
-
-        [Required]
-        public string Name { get; set; }
-
-        [Required]
         public int BudgetRowId { get; set; }
 
         [Required]
@@ -19,9 +13,18 @@ namespace HomeApp.DataAccess.Models
         [Required]
         public int BudgetGroupId { get; set; }
 
-        public virtual User User { get; set; }
+        [Required]
+        public int UserId { get; set; }
+        
+        [Required]
+        public int Year { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
         public virtual BudgetColumn BudgetColumn { get; set; }
         public virtual BudgetGroup BudgetGroup { get; set; }
         public virtual BudgetRow BudgetRow { get; set; }
+        public virtual User User { get; set; }
     }
 }
