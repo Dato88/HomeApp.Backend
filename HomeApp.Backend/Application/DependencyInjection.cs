bdd0c606ecc4dfca696242186c746e98a5cfd0f5
Andrej Miller <a.miller@p-c-m.de>
2025-03-25 21:52:23 +0100

    created dependency injections

---
+++ HomeApp.Backend/Application/DependencyInjection.cs
@@ -0,0 +1,49 @@
+﻿using Application.Common.Interfaces.People;
+using Application.Common.Interfaces.Todos;
+using Application.Common.People;
+using Application.Common.People.Validations;
+using Application.Common.People.Validations.Interfaces;
+using Application.Common.Todos;
+using Application.Email;
+using Application.Models.Email;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services, IConfiguration configuration) =>
+        services
+            .AddMediatR()
+            .AddServices(configuration);
+
+    private static IServiceCollection AddMediatR(this IServiceCollection services)
+    {
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(AssemblyReference).Assembly));
+
+        return services;
+    }
+
+    private static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        // services.AddScoped<IBudgetValidation, BudgetValidation>();
+        services.AddScoped<IPersonValidation, PersonValidation>();
+
+        // services.AddScoped<IBudgetCellCrud, BudgetCellQueries>();
+        // services.AddScoped<IBudgetColumnCrud, BudgetColumnQueries>();
+        // services.AddScoped<IBudgetGroupCrud, BudgetGroupQueries>();
+        // builder.Services.AddScoped<IBudgetRowCrud, BudgetRowQueries>();
+        // builder.Services.AddScoped<IBudgetFacade, BudgetFacade>();
+        services.AddScoped<ICommonPersonCommands, CommonPersonCommands>();
+        services.AddScoped<ICommonPersonQueries, CommonPersonQueries>();
+        services.AddScoped<ITodoCommands, TodoCommands>();
+        services.AddScoped<ITodoQueries, TodoQueries>();
+
+        var emailConfig = configuration.GetSection("EmailConfiguration").Get<EmailConfiguration>();
+        services.AddSingleton(emailConfig);
+        services.AddScoped<IEmailSender, EmailSender>();
+
+        return services;
+    }
+}
---
+++ HomeApp.Backend/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,146 @@
+﻿using System.Text;
+using Domain.Entities.User;
+using Infrastructure.Authorization.Handler;
+using Infrastructure.Authorization.Utilities;
+using Infrastructure.Database;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Serilog;
+
+namespace Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration) =>
+        services
+            .AddServices()
+            .AddDatabase(configuration)
+            .AddCors()
+            .AddHealthChecks(configuration)
+            .AddAuthenticationInternal(configuration)
+            .AddAuthorizationInternal();
+
+    private static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        ILogger logger =
+            new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+
+        services.AddSingleton(logger);
+
+        return services;
+    }
+
+    private static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<HomeAppContext>(options =>
+            options.UseNpgsql(
+                    configuration.GetConnectionString("HomeAppConnection"),
+                    npgsqlOptions =>
+                    {
+                        npgsqlOptions.MigrationsHistoryTable("__ef_migrations_homeapp", "public");
+                    })
+                .UseSnakeCaseNamingConvention());
+
+        services.AddDbContext<UserContext>(options =>
+            options.UseNpgsql(
+                    configuration.GetConnectionString("HomeAppUserConnection"),
+                    npgsqlOptions =>
+                    {
+                        npgsqlOptions.MigrationsHistoryTable("__ef_migrations_user", "public");
+                    })
+                .UseSnakeCaseNamingConvention());
+
+
+        return services;
+    }
+
+    private static IServiceCollection AddCors(this IServiceCollection services)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy", policy => policy
+                .WithOrigins("http://localhost:4200", "https://localhost:4200")
+                .AllowAnyHeader()
+                .AllowAnyMethod());
+        });
+
+        return services;
+    }
+
+    private static IServiceCollection AddHealthChecks(this IServiceCollection services, IConfiguration configuration)
+    {
+        services
+            .AddHealthChecks()
+            .AddNpgSql(configuration.GetConnectionString("HomeAppConnection")!);
+        return services;
+    }
+
+    private static IServiceCollection AddAuthenticationInternal(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var jwtSettings = configuration.GetSection("JwtSettings");
+
+        services.AddAuthentication(opt =>
+        {
+            opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = jwtSettings["validIssuer"],
+                ValidAudience = jwtSettings["validAudience"],
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
+                    .GetBytes(jwtSettings.GetSection("securityKey").Value))
+            };
+        });
+
+        services.AddHttpContextAccessor();
+
+        return services;
+    }
+
+    private static IServiceCollection AddAuthorizationInternal(this IServiceCollection services)
+    {
+        services.AddAuthorization(options =>
+        {
+            foreach (var item in ClaimStore.AllClaims)
+                options.AddPolicy($"{item.Type.Replace(" ", "")}Policy", policy => policy.RequireClaim(item.Value));
+        });
+
+        services.AddScoped<JwtHandler>();
+
+        services.AddIdentity<User, IdentityRole>(
+                opt =>
+                {
+                    opt.Password.RequiredLength = 7;
+                    opt.Password.RequireDigit = false;
+
+                    opt.User.RequireUniqueEmail = true;
+
+                    opt.Lockout.AllowedForNewUsers = true;
+                    opt.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(10);
+                    opt.Lockout.MaxFailedAccessAttempts = 3;
+                })
+            .AddEntityFrameworkStores<UserContext>()
+            .AddDefaultTokenProviders();
+
+        services.Configure<DataProtectionTokenProviderOptions>(opt =>
+            opt.TokenLifespan = TimeSpan.FromHours(1));
+
+        return services;
+    }
+}
