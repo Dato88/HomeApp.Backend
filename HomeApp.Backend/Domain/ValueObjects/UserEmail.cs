8756a3528e7b506272cc3c72bf5546977a939d68
Andrej Miller <andrej_miller@outlook.de>
2025-07-13 11:32:12 +0200

    updated UserEmail Value Object

--- HomeApp.Backend/Domain/ValueObjects/UserEmail.cs
+++ HomeApp.Backend/Domain/ValueObjects/UserEmail.cs
@@ -1,3 +1,33 @@
-﻿namespace Domain.ValueObjects;
+﻿using System.Net.Mail;
 
-public readonly record struct UserEmail(string Value);
+namespace Domain.ValueObjects;
+
+public readonly record struct UserEmail
+{
+    public UserEmail(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            throw new ArgumentException($"'{nameof(value)}' cannot be null or whitespace.", nameof(value));
+
+        if (!IsValidEmail(value))
+            throw new ArgumentException($"'{nameof(value)}' is not a valid email address.", nameof(value));
+
+        Value = value;
+    }
+
+    public string Value { get; }
+
+    private static bool IsValidEmail(string email)
+    {
+        try
+        {
+            var mailAdress = new MailAddress(email);
+
+            return mailAdress.Address == email;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
