a982d078bd7bb3b7e63d4017cddd9ef5f5161455
Andrej Miller <andrej_miller@outlook.de>
2025-09-28 09:27:06 +0200

    chore: removed unused tests

--- HomeApp.Backend/ApplicationTests/Crud/TodoGroupTodoCrudTests/BaseTodoGroupTodoCrudTest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HomeApp.Library.Tests.Crud.TodoGroupTodoCrudTests;
-
-public class BaseTodoGroupTodoCrudTest
-{
-    // protected readonly TodoGroupTodoCrud _todoGroupTodoCrud;
-    //
-    // public BaseTodoGroupTodoCrudTest(UnitTestingApiFactory unitTestingApiFactory) : base(unitTestingApiFactory) =>
-    //     _todoGroupTodoCrud = new TodoGroupTodoCrud(DbContext);
-}
--- HomeApp.Backend/ApplicationTests/Crud/TodoPersonCrudTests/BaseTodoPersonCrudTest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HomeApp.Library.Tests.Crud.TodoPersonCrudTests;
-
-public class BaseTodoPersonCrudTest
-{
-    // protected readonly TodoPersonCrud _todoPersonCrud;
-    //
-    // public BaseTodoPersonCrudTest(UnitTestingApiFactory unitTestingApiFactory) : base(unitTestingApiFactory) =>
-    //     _todoPersonCrud = new TodoPersonCrud(DbContext);
-}
