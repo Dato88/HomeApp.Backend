48598ec6752a20b3a2d89c8817cf9a86dcf58e1e
Andrej Miller <andrej_miller@outlook.de>
2025-09-28 09:15:12 +0200

    feat: new Create Budget Validators

---
+++ HomeApp.Backend/Application/Features/Budgets/Commands/Create/CreateBudgetCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Application.Features.Budgets.Commands.Create;
+
+public class CreateBudgetCommandValidator : AbstractValidator<CreateBudgetCommand>
+{
+    public CreateBudgetCommandValidator()
+    {
+        RuleFor(c => c.Year)
+            .InclusiveBetween(1900, DateTime.UtcNow.Year + 10)
+            .WithMessage("Year must be between 1900 and " + (DateTime.UtcNow.Year + 10) + ".");
+    }
+}
---
+++ HomeApp.Backend/Application/Features/Budgets/Commands/Create/CreateBudgetRowCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Application.Features.Budgets.Commands.Create;
+
+internal sealed class CreateBudgetRowCommandValidator : AbstractValidator<CreateBudgetRowCommand>
+{
+    public CreateBudgetRowCommandValidator()
+    {
+        RuleFor(c => c.BudgetGroupId).GreaterThan(0);
+        RuleFor(c => c.Index).GreaterThan(0);
+        RuleFor(c => c.Name).NotEmpty()
+            .WithMessage("Name is required.");
+    }
+}
