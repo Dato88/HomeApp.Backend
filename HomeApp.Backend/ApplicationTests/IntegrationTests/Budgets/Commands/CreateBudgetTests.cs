434279515cc436fac268af7d2bc91b2b2f9973c3
Andrej Miller <andrej_miller@outlook.de>
2025-09-30 20:11:34 +0200

    test: new CreateBudget_ShouldCreateNewBudget and implementation of Budget Seeder

---
+++ HomeApp.Backend/ApplicationTests/IntegrationTests/Budgets/Commands/CreateBudgetTests.cs
@@ -0,0 +1,27 @@
+﻿using Application.Abstractions.Authentication;
+
+namespace ApplicationTests.IntegrationTests.Budgets.Commands;
+
+public class CreateBudgetTests : BaseBudgetCommandsTest
+{
+    public CreateBudgetTests(UnitTestingApiFactory unitTestingApiFactory) : base(unitTestingApiFactory)
+    {
+    }
+
+    [Fact]
+    public async Task CreateBudget_ShouldCreateNewBudget()
+    {
+        // Arrange
+        var year = 2030;
+
+        // Act
+        var result = await BudgetCommands.CreateBudgetAsync(year, CancellationToken.None);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        var created = await DbContext.Budgets.FindAsync(result.Value);
+        created.Should().NotBeNull();
+        created!.Year.Should().Be(year);
+        created.PersonId.Should().Be(UserContext.PersonId);
+    }
+}
---
+++ HomeApp.Backend/ApplicationTests/IntegrationTests/TestData/BudgetDataSeeder.cs
@@ -0,0 +1,31 @@
+﻿using Bogus;
+using Domain.Entities.Budgets;
+
+namespace ApplicationTests.IntegrationTests.TestData;
+
+public class BudgetDataSeeder : BaseTest
+{
+    private readonly PeopleDataSeeder _peopleDataSeeder;
+
+    private readonly Faker<Budget> _budgetFaker;
+
+    public BudgetDataSeeder(UnitTestingApiFactory unitTestingApiFactory) : base(unitTestingApiFactory)
+    {
+        _peopleDataSeeder = new PeopleDataSeeder(unitTestingApiFactory);
+
+        _budgetFaker = new Faker<Budget>()
+            .RuleFor(u => u.Year, f => f.Date.Recent().Year)
+            .RuleFor(u => u.CreatedAt, f => f.Date.RecentOffset(10).UtcDateTime);
+    }
+
+    public async Task<Budget> GenereateDummyTodo(int? personId = null)
+    {
+        await Task.Delay(0);
+
+        var budget = _budgetFaker.Generate();
+
+        if (personId.HasValue) budget.PersonId = personId.Value;
+
+        return budget;
+    }
+}
