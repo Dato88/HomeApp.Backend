ac6bbf3687db30fc8b46fbec100eb39b0b403c32
Andrej Miller <andrej_miller@outlook.de>
2025-09-30 21:54:11 +0200

    test: new CreateBudgetRowTests

---
+++ HomeApp.Backend/ApplicationTests/IntegrationTests/Budgets/Commands/CreateBudgetRowTests.cs
@@ -0,0 +1,54 @@
+﻿using Domain.Entities.Budgets;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApplicationTests.IntegrationTests.Budgets.Commands;
+
+public class CreateBudgetRowTests : BaseBudgetCommandsTest
+{
+    public CreateBudgetRowTests(UnitTestingApiFactory unitTestingApiFactory) : base(unitTestingApiFactory)
+    {
+    }
+
+    [Theory]
+    [InlineData(1, 0, 0, "Group Row Name")]
+    [InlineData(5, 3, 3, "Group Row Name")]
+    [InlineData(7, 5, 7, "Group Row Name")]
+    public async Task CreateBudgetGroup_ShouldCreateNewBudgetGroup(int groupCount, int selectedGroupIndex, int index,
+        string name)
+    {
+        // Arrange
+        var newBudget = await BudgetDataSeeder.GenereateDummyBudgetGroups(groupCount, UserContext.PersonId);
+
+        var groupIds = await DbContext.BudgetGroups
+            .Where(g => g.BudgetId == newBudget.BudgetId)
+            .OrderBy(g => g.Index) // oder .OrderBy(g => g.BudgetGroupId)
+            .Select(g => g.BudgetGroupId)
+            .ToListAsync();
+
+        var newBudgetRow = new BudgetRow { BudgetGroupId = groupIds[selectedGroupIndex], Index = index, Name = name };
+
+        // Act
+        var result = await BudgetCommands.CreateBudgetRowAsync(newBudgetRow, CancellationToken.None);
+
+        // Assert
+        var created = await DbContext.BudgetRows.FindAsync(result.Value);
+        created.Should().NotBeNull();
+        created.Index.Should().Be(newBudgetRow.Index);
+        created.Name.Should().Be(newBudgetRow.Name);
+    }
+
+    [Fact]
+    public async Task CreateBudgetRow_ShouldReturnErrorWhenBudgetIdIsInvalid()
+    {
+        // Arrange
+        var newBudgetRow = new BudgetRow { BudgetGroupId = 0, Index = 0, Name = "Test Budget Row" };
+
+        // Act
+        var result = await BudgetCommands.CreateBudgetRowAsync(newBudgetRow, CancellationToken.None);
+
+        // Assert
+        result.IsSuccess.Should().BeFalse();
+        result.IsFailure.Should().BeTrue();
+        result.Error.Should().Be(BudgetErrors.CreateFailedWithMessage("BudgetGroupId is invalid"));
+    }
+}
--- HomeApp.Backend/ApplicationTests/IntegrationTests/TestData/BudgetDataSeeder.cs
+++ HomeApp.Backend/ApplicationTests/IntegrationTests/TestData/BudgetDataSeeder.cs
@@ -1,5 +1,6 @@
 ﻿using Bogus;
 using Domain.Entities.Budgets;
+using Domain.Entities.Budgets.Enums;
 
 namespace ApplicationTests.IntegrationTests.TestData;
 
@@ -8,6 +9,7 @@ public class BudgetDataSeeder : BaseTest
     private readonly PeopleDataSeeder _peopleDataSeeder;
 
     private readonly Faker<Budget> _budgetFaker;
+    private readonly Faker<BudgetGroup> _budgetGroupFaker;
 
     public BudgetDataSeeder(UnitTestingApiFactory unitTestingApiFactory) : base(unitTestingApiFactory)
     {
@@ -15,6 +17,11 @@ public class BudgetDataSeeder : BaseTest
 
         _budgetFaker = new Faker<Budget>()
             .RuleFor(u => u.Year, f => f.Date.Recent().Year)
+            .RuleFor(u => u.CreatedAt, f => f.Date.RecentOffset(10).UtcDateTime);
+
+        _budgetGroupFaker = new Faker<BudgetGroup>()
+            .RuleFor(u => u.Name, f => f.Lorem.Word())
+            .RuleFor(u => u.BudgetGroupType, f => f.PickRandom<BudgetGroupType>())
             .RuleFor(u => u.CreatedAt, f => f.Date.RecentOffset(10).UtcDateTime);
     }
 
@@ -27,6 +34,25 @@ public class BudgetDataSeeder : BaseTest
         if (saveAsync)
         {
             await DbContext.Budgets.AddAsync(budget);
+            await DbContext.SaveChangesAsync();
+        }
+
+        return budget;
+    }
+
+    public async Task<Budget> GenereateDummyBudgetGroups(int budgetGroupCount, int? personId = null,
+        bool saveAsync = true)
+    {
+        var budget = await GenereateDummyBudget(personId, saveAsync);
+
+        for (var i = 0; i < budgetGroupCount; i++)
+        {
+            var newBudgetGroup = _budgetGroupFaker.Generate();
+            newBudgetGroup.BudgetId = budget.BudgetId;
+            newBudgetGroup.Index = i;
+            newBudgetGroup.CreatedById = budget.CreatedById;
+
+            await DbContext.BudgetGroups.AddAsync(newBudgetGroup);
             await DbContext.SaveChangesAsync();
         }
 
