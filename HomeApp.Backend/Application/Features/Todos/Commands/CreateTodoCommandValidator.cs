e998c22b229c4836f9358ab2ace03025dae46d70
Andrej Miller <andrej_miller@outlook.de>
2025-10-04 11:19:37 +0200

    feat: new CreateTodoCommandValidator for not empty Names

---
+++ HomeApp.Backend/Application/Features/Todos/Commands/CreateTodoCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace Application.Features.Todos.Commands;
+
+public class CreateTodoCommandValidator : AbstractValidator<CreateTodoCommand>
+{
+    public CreateTodoCommandValidator()
+    {
+        RuleFor(c => c.Name).NotEmpty().WithMessage("Todo name is required");
+    }
+}
