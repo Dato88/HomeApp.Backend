8501a20f37e81d5e8d3120acaceeaa2ded06c795
Andrej Miller <andrej_miller@outlook.de>
2026-06-28 12:33:07 +0200

    feat: added authorize policy for budget and todo

--- HomeApp.Backend/Web.Api/Controllers/BudgetController.cs
+++ HomeApp.Backend/Web.Api/Controllers/BudgetController.cs
@@ -10,7 +10,7 @@ using Web.Api.Requests.Budget;
 namespace Web.Api.Controllers;
 
 [ApiController]
-[Authorize]
+[Authorize(Policy = "ViewBudgetPolicy")]
 [Route("[controller]")]
 public class BudgetController(IMediator mediator) : ControllerBase
 {
